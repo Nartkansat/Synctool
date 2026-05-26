4c091d81c56052d2e485976d435c1af221ade9b4
nartkansat <nartkansat@gmail.com>
2026-05-26 12:20:49 +0300

    Version v1.0.0

--- Services/UpdateService.cs
+++ Services/UpdateService.cs
@@ -6,7 +6,7 @@ namespace Synctool.Services
     public static class UpdateService
     {
         // GitHub deponuz Public olduğu için direkt raw URL kullanıyoruz.
-        private const string UpdateXmlUrl = "https://raw.githubusercontent.com/Nartkansat/ArcelikApp/main/update.xml";
+        private const string UpdateXmlUrl = "https://raw.githubusercontent.com/Nartkansat/Synctool/main/update.xml";
 
         public static void CheckForUpdates()
         {
